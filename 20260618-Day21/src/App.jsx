92b6d4b9addb4ce337e50c37f4ec3d8dbc21799f
shinhyunho7653 <awd2633@gmail.com>
2026-06-18 15:18:00 +0900

    오늘처음배운리엑트

---
+++ 20260618-Day21/src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h2>화면 깜빡임 없음</h2>
+      <h1>처음 배우는 리엑트</h1>
+      <p>리엑트는 페이지를 이동할 때마다 새로고침이 돼서 기존 내용들이 모두 리셋된다</p>
+      <p>리엑트는 HTMl 파일이 하나다</p>
+
+      <h2>Single Page Application</h2>
+      <p>html 파일 1개만 사용. 다른 페이지를 보여줄 때는 html 부분만 갈아서 보여준다</p>
+      <p>test</p>
+      
+    </>
+  )
+}
+
+export default App
---
+++ 20260618-Day21/src/App2.jsx
@@ -0,0 +1,13 @@
+
+
+function App2(){
+
+  return (
+    <div>
+      <h1>새로운 태그 만들기</h1>
+    </div>
+  )
+}
+// 내보내기
+// 내가 직접 만든 태그를 다른 파일에 보내기
+export default App2;
